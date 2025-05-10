743197966a6d2ec2fbf374d670315bd31bea90c6
Artur Klesun <ArturKlesun@gmail.com>
2025-05-10 12:29:56 +0300

    Add files via upload

---
+++ generate_speeches.js
@@ -0,0 +1,41 @@
+import fs from "fs/promises";
+
+/** @kudos to https://stackoverflow.com/a/27979933/2750743 */
+function escapeXml(unsafe) {
+    return unsafe.replace(/[<>&'"]/g, function (c) {
+        switch (c) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '\'': return '&apos;';
+            case '"': return '&quot;';
+        }
+    });
+}
+
+async function generateSpeech(msSpeakerId, text) {
+    const response = await fetch("https://swedencentral.tts.speech.microsoft.com/cognitiveservices/v1", {
+        method: "POST",
+        headers: {
+            "X-Microsoft-OutputFormat": "ogg-48khz-16bit-mono-opus",
+            "Ocp-Apim-Subscription-Key": "2454960cc6ff432b961e84cd3dd724f2",
+            "Content-Type": "application/ssml+xml",
+        },
+        body: `<speak version='1.0' xml:lang='ru-RU'>
+            <voice xml:lang='ru-RU' name='${msSpeakerId}'>${escapeXml(text)}</voice>
+        </speak>`,
+    });
+    if (!response.ok) {
+        throw new Error("Unsuccessful status returned by speech API: " + response.status + " speaking of " + text);
+    }
+    return await response.arrayBuffer();
+}
+
+for (const paragraph of chapter.paragraphs) {
+    let { id, speaker, text } = paragraph;
+    console.log("#" + id + " " + speaker + ": " + text);
+    const msSpeakerId = "ru-RU-DmitryNeural";
+    const speechOggBuff = await generateSpeech(msSpeakerId, text);
+    const outputFileName = id + ".oga";
+    await fs.appendFile("./speeches/" + outputFileName, Buffer.from(speechOggBuff));
+}
